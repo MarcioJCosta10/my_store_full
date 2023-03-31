ffe6ea880e2f2eab9bddd714d02c9bc0d8f31ded
“MarcioJCosta10” <marciojcostadev@gmail.com>
2023-03-30 22:15:34 -0300

    add path store in my_store_full/urls.py and create newq path to views.store in store/urls.py

--- my_store_full/urls.py
+++ my_store_full/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from . import views
 from django.conf.urls.static import static
 from django.conf import settings
@@ -7,5 +7,6 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('store/', include('store/urls.py'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
