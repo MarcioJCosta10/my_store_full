18eceed5c7deb734071209fd4b27169d51b8d204
“MarcioJCosta10” <marciojcostadev@gmail.com>
2023-04-12 23:07:55 -0300

    define function get_url in category/models.py

--- category/models.py
+++ category/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 class Category(models.Model):
     category_name = models.CharField(max_length=50, unique=True)
@@ -10,6 +11,10 @@ class Category(models.Model):
         verbose_name = 'category'
         verbose_name_plural = 'categories'
         
+        # self is passed because we be using functions into class methods
+        def get_url(self):
+            return reverse('product_by_category', args=[self.slug])
+            
     
     def __str__(self):
         return self.category_name
