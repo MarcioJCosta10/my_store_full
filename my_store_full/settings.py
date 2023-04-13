5520bb7db4deeadb6d72535a5b34c94e351e9540
“MarcioJCosta10” <marciojcostadev@gmail.com>
2023-04-12 22:58:13 -0300

    insert loop to list all cateregory in templates/includes/navbar.html

--- my_store_full/settings.py
+++ my_store_full/settings.py
@@ -67,6 +67,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                # menu links to use in all templates
+                'django.template.context_processors.menu_links',
             ],
         },
     },
