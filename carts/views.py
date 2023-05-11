b0b318426a0f95ed1ac0c7e485f3fd0400dec179
“MarcioJCosta10” <marciojcostadev@gmail.com>
2023-05-10 22:13:26 -0300

    test

--- carts/views.py
+++ carts/views.py
@@ -1,9 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from store.models import Product
+from .models import Cart, CartItem
 
+#_ underscore before name functions is to private function
 
+def _cart_id(request):
+    cart = request.session.session_key
+    if not cart:
+        cart = request.session.create()
+    return cart
+        
+
+def add_cart(request, product_id):
+    product = Product.objects.get(id=product_id)
+    try:
+        cart = Cart.objects.get(cart_id=_cart_id(request)) # get the cart_id using cart_id present in the session 
+    except Cart.DoesNotExist:
+        cart = Cart.objects.create(
+            cart_id = _cart_id(request)
+        )
+    cart.save() 
+    
+    #combine product and cart to we get the item from cart
+    try:        
+        cart_item = CartItem.objects.get(product=product, cart=cart)
+        cart_item.quantity += 1 #cart_item.quantity =  cart_item.quantity + 1
+        cart_item.save()
+    except CartItem.DoesNotExist:
+        cart_item = CartItem.objects.create(
+            product = product, 
+            quantity = 1,
+            cart=cart
+            )
+        cart_item.save()
+    return redirect('cart')       
 
 # Create your views here.art
-def cart(request):
-    
+def cart(request):    
     return render(request, 'store/cart.html')
     
