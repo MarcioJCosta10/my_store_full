c30c7f45033edf6911e80fcfe5e063ddee62518d
“MarcioJCosta10” <marciojcostadev@gmail.com>
2023-07-07 21:16:39 -0300

    create fields variation in admin with list_diplay and variationAdmin in store/admin.py

--- store/admin.py
+++ store/admin.py
@@ -4,9 +4,12 @@ from .models import Product, Variation
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('product_name', 'price', 'stock', 'category', 'modified_date', 'is_available')
     prepopulated_fields = {'slug': ('product_name', )}
+
+class variationAdmin(admin.ModelAdmin):
+    list_display = ('product', 'variation_category', 'variation_value', 'is_active')
     
 # Register your models here.
 
 admin.site.register(Product, ProductAdmin)
-admin.site.register(Variation)
+admin.site.register(Variation, variationAdmin)
 
